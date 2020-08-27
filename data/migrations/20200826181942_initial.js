b6f86c580e34aa4333ed8e021538581a55b624a4
RyanMersmann <mersmannryan@gmail.com>
2020-08-27 16:41:07 -0600

    done

---
+++ data/migrations/20200826181942_initial.js
@@ -0,0 +1,35 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("zoos", (table) => {
+      table.increments("id")
+      table.text("name").notNullable()
+      table.text("address").notNullable().unique()
+  })
+
+  await knex.schema.createTable("species", (table) => {
+    table.increments("id")
+    table.text("name").notNullable().unique()
+  })
+
+  await knex.schema.createTable("animals", (table) => {
+    table.increments("id")
+    table.text("name").notNullable()
+    table.integer("species_id").references("id").inTable("species")
+  })
+
+  await knex.schema.createTable("zoos_animals", (table) => {
+    table.integer("zoo_id").references("id").inTable("zoos").notNullable()
+    table.text("animal_id").references("id").inTable("animals").notNullable()
+    table.date("from_date").notNullable().defaultTo(knex.raw("current_timestamp"))
+    table.date("to_date")
+
+    table.primary(["zoo_id", "animal_id"])
+  })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("zoos_animals")
+    await knex.schema.dropTableIfExists("animals")
+    await knex.schema.dropTableIfExists("species")
+    await knex.schema.dropTableIfExists("zoos")
+};
--- models/zoo.js
+++ models/zoo.js
@@ -10,7 +10,24 @@ function findById(id) {
 		.first()
 }
 
+function findAnimals(zooID) {
+	// Select * from animals where zoo_id = ?
+	return db("zoos_animals as za")
+		.join("zoos as z", "z.id", "za.zoo_id")
+		.join("animals as a", "a.id", "za.animal_id")
+		.join("species as s", "s.id", "a.species_id")
+		.where("z.id", zooID)
+		.select(
+			"a.id",
+			"a.name",
+			"s.name as species_name",
+			"za.from_date",
+			"za.to_date",
+		)
+}
+
 module.exports = {
 	find,
 	findById,
+	findAnimals,
 }
