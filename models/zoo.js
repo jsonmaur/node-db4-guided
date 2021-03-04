fb2f59e1e3ddf2fa6deec2a0e8bbb95332ae32b5
Jason Maurer <jsonmaur@fastmail.com>
2021-03-03 19:39:17 -0700

    Code from guided lecture

--- models/zoo.js
+++ models/zoo.js
@@ -10,7 +10,24 @@ function findById(id) {
 		.first()
 }
 
+function findAnimals(zooID) {
+	return db("zoos_animals as za")
+		// this join is only necessary if selecting columns from `zoos`
+		// .innerJoin("zoos as z", "z.id", "za.zoo_id")
+		.innerJoin("animals as a", "a.id", "za.animal_id")
+		.innerJoin("species as s", "s.id", "a.species_id")
+		.where("za.zoo_id", zooID)
+		.select(
+			"a.id",
+			"a.name",
+			"s.name as species_name",
+			"za.arrival",
+			"za.departure",
+		)
+}
+
 module.exports = {
 	find,
 	findById,
+	findAnimals,
 }
